0ee96f809ce3e95165af315d04043a8a0533bc3c
larakoseff <larakoseff@gmail.com>
2023-08-08 20:12:49 +0200

    make initial code edits

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+// clue: location should be changed to country 
+
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
+
+if (location = RSA) { shipping === 400 && currency === 'R' }
+
+if (location = NAM){
+shipping = 600} 
+else {shipping = 800}
+
+const shoes = 300 * 1
+const toys = - 100 * 5
+const shirts = 150 * 'NONE_SELECTED'
+const batteries = 35 * 2
+const pens = 5 * 'NONE_SELECTED' 
+
+shipping = null
+currency = $
+
+if (shoes + batteries + pens + shirts > 1000 && ) {
+	if (location = NAM && customers < 2)
+			if (location = RSA)
+		    {shipping = 0 || calcShipping
+		}
+	}
+}
+
+if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+
+location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+
+customers = '1'
+const location = 'RSA'
+currency = null
