6499902cbc8ff12371bf9c3c677d05e05271c107
larakoseff <larakoseff@gmail.com>
2023-08-08 22:03:21 +0200

    solve challenge (console log correct)

--- scripts.js
+++ scripts.js
@@ -1,37 +1,48 @@
+const FREE_WARNING = "Free shipping only applies to single customer orders";
+const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
+const NONE_SELECTED = "0";
 
-// clue: location should be changed to country 
+let customers = 1;
+let country = "RSA";
+let currency = null;
+let shipping = null;
 
-const FREE_WARNING = 'Free shipping only applies to single customer orders'
-const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-const NONE_SELECTED = '0'
+let shoes = 300 * 1;
+let toys = 100 * 5;
+let shirts = 150 * NONE_SELECTED;
+let batteries = 35 * 2;
+let pens = 5 * NONE_SELECTED;
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+let price = shoes + toys + batteries + pens + shirts;
 
-if (location = NAM){
-shipping = 600} 
-else {shipping = 800}
-
-const shoes = 300 * 1
-const toys = - 100 * 5
-const shirts = 150 * 'NONE_SELECTED'
-const batteries = 35 * 2
-const pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-currency = $
-
-if (shoes + batteries + pens + shirts > 1000 && ) {
-	if (location = NAM && customers < 2)
-			if (location = RSA)
-		    {shipping = 0 || calcShipping
-		}
-	}
+if (country == "RSA" && price < 1000) {
+  currency = "R";
+  shipping = 400;
+} else if (country == "RSA" && customers == 1 && price >= 1000) {
+  currency = "R";
+  shipping = 0;
+} else if (country == "NAM" && price < 1000) {
+  currency = "$";
+  shipping = 600;
+} else if (
+  country == "NAM" &&
+  currency == "$" &&
+  customers == 1 &&
+  price >= 1000
+) {
+  shipping = 0;
+} else if (country == "NK") {
+  console.log(BANNED_WARNING);
+} else {
+  shipping == 800 && currency == "$";
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+if (price > 60 && currency == "$" && country == "NAM" && customers == 1) {
+  let shipping = 0;
+}
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+if (shipping === 0 && customers !== 1) {
+  console.log(FREE_WARNING);
+}
 
-customers = '1'
-const location = 'RSA'
-currency = null
+console.log(currency, price + shipping);
