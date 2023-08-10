78394e13bdc708a3c9ef7e2251d1974f4871d7a8
larakoseff <larakoseff@gmail.com>
2023-08-10 10:56:00 +0200

    Fix NK issue after peer group meeting

--- scripts.js
+++ scripts.js
@@ -1,45 +1,47 @@
 const FREE_WARNING = "Free shipping only applies to single customer orders";
 const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
-const NONE_SELECTED = "0";
+const NONE_SELECTED = 0;
 
-let customers = 1;
-let country = "RSA";
+const customers = 1;
+const country = "RSA";
 let currency = null;
 let shipping = null;
 
-let shoes = 300 * 1;
-let toys = 100 * 5;
-let shirts = 150 * NONE_SELECTED;
-let batteries = 35 * 2;
-let pens = 5 * NONE_SELECTED;
+const shoes = 300 * 1;
+const toys = 100 * 5;
+const shirts = 150 * NONE_SELECTED;
+const batteries = 35 * 2;
+const pens = 5 * NONE_SELECTED;
 
-let price = shoes + toys + batteries + pens + shirts;
+const price = shoes + toys + batteries + pens + shirts;
 
-if (country == "RSA" && price < 1000) {
+if (country === "RSA" && price < 1000) {
   currency = "R";
   shipping = 400;
-} else if (country == "RSA" && customers == 1 && price >= 1000) {
+} else if (country === "RSA" && customers === 1 && price >= 1000) {
   currency = "R";
   shipping = 0;
-} else if (country == "NAM" && price < 60) {
+} else if (country === "NAM" && price < 60) {
   currency = "$";
   shipping = 600;
-} else if (country == "NAM" && customers == 1 && price >= 60) {
+} else if (country === "NAM" && customers === 1 && price >= 60) {
 currency = "$";
 shipping = 0;
-} else if (country == "NK") {
-  console.log(BANNED_WARNING);
-  currency = null;
-  shipping = null;
-  price = null;
 } else {
 shipping = 800; 
 currency = "$";
 }
 
-
 if (shipping === 0 && customers !== 1) {
   console.log(FREE_WARNING);
 }
 
-console.log(currency, price + shipping);
+ if (country === "NK") {
+  console.log(BANNED_WARNING);
+ }
+
+ else {
+  console.log(currency, price + shipping);
+}
+
+
